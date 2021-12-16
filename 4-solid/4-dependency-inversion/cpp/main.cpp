990420daa8ed92268033356a8aca1002ad117fa2
kphuang <kphuang@gmail.com>
2021-12-17 04:20:17 +0800

    added DIP example

--- 4-solid/4-dependency-inversion/cpp/main.cpp
+++ 4-solid/4-dependency-inversion/cpp/main.cpp
@@ -1,2 +1,83 @@
 // 1. higher level module should not depend on lower level module, both should depend on abstract interfaces.
 // 2. abstract interface should not depend on concrete implementaion. concrete implementation should depend on abstract interface.
+
+// obeys DIP
+class IDatabase {
+public:
+    virtual void connect() = 0;
+    virtual void crud() = 0;
+    virtual void disconnect() = 0;
+};
+
+class MySQL: public IDatabase {
+public:
+    MySQL() {}
+    void connect() override {}
+    void crud() override {}
+    void disconnect() override {}
+};
+
+class PostgreSQL: public IDatabase {
+public:
+    PostgreSQL() {}
+    void connect() override {}
+    void crud() override {}
+    void disconnect() override {}
+};
+
+class IMonitor {
+public:
+    virtual void start() = 0;
+    virtual void getData() = 0;
+    virtual void stop() = 0;
+};
+
+class NetworkMonitor: public IMonitor {
+public:
+    NetworkMonitor() {}
+    void start() override {}
+    void getData() override {}
+    void stop() override {}
+};
+
+class IService {
+public:
+    virtual void start() = 0;
+    virtual void check() = 0;
+    virtual void stop() = 0;
+    virtual void setDB(IDatabase *db) = 0;
+};
+
+class NetPerfService: public IService {
+    IDatabase *_db;
+    IMonitor *_m;
+public:
+    NetPerfService(IDatabase *db, IMonitor *m): _db(db), _m(m) {}
+    void start() {
+        _db->connect();
+        _m->start();
+    }
+    void check() {
+        _m->getData();
+        _db->crud();
+    }
+    void stop() {
+        _m->stop();
+        _db->disconnect();
+    }
+    void setDB(IDatabase *db) {
+        _db = db;
+    }
+};
+
+int main() {
+    IService *s = new NetPerfService{new MySQL{}, new NetworkMonitor{}};
+    s->start();
+    s->check();
+    s->stop();
+    s->setDB(new PostgreSQL{});
+    s->start();
+    s->check();
+    s->stop();
+    return 0;
+}
