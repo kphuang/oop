c94ddf0aaef98145eaeb218e0f0ca5b4234f8208
kphuang <kphuang@gmail.com>
2021-12-18 03:44:34 +0800

    added DIP vialation example

--- 4-solid/4-dependency-inversion/cpp/main.cpp
+++ 4-solid/4-dependency-inversion/cpp/main.cpp
@@ -1,6 +1,19 @@
 // 1. higher level module should not depend on lower level module, both should depend on abstract interfaces.
 // 2. abstract interface should not depend on concrete implementaion. concrete implementation should depend on abstract interface.
 
+// violates DIP
+class MySQL0 {};
+class NetworkMonitor0 {};
+class NetPerfService0 {
+    MySQL0 mySQL;
+    NetworkMonitor0 networkMonitor;
+public:
+    NetPerfService0() {}
+    void start() {}
+    void check() {}
+    void stop() {}
+};
+
 // obeys DIP
 class IDatabase {
 public:
@@ -40,38 +53,50 @@ public:
     void stop() override {}
 };
 
+class MemoryMonitor: public IMonitor {
+public:
+    MemoryMonitor() {}
+    void start() override {}
+    void getData() override {}
+    void stop() override {}
+};
+
 class IService {
 public:
     virtual void start() = 0;
     virtual void check() = 0;
     virtual void stop() = 0;
     virtual void setDB(IDatabase *db) = 0;
+    virtual void setMonitor(IMonitor *m) = 0;
 };
 
-class NetPerfService: public IService {
+class PerfService: public IService {
     IDatabase *_db;
     IMonitor *_m;
 public:
-    NetPerfService(IDatabase *db, IMonitor *m): _db(db), _m(m) {}
-    void start() {
+    PerfService(IDatabase *db, IMonitor *m): _db(db), _m(m) {}
+    void start() override {
         _db->connect();
         _m->start();
     }
-    void check() {
+    void check() override {
         _m->getData();
         _db->crud();
     }
-    void stop() {
+    void stop() override {
         _m->stop();
         _db->disconnect();
     }
-    void setDB(IDatabase *db) {
+    void setDB(IDatabase *db) override {
         _db = db;
+    }
+    void setMonitor(IMonitor *m) override {
+        _m = m;
     }
 };
 
 int main() {
-    IService *s = new NetPerfService{new MySQL{}, new NetworkMonitor{}};
+    IService *s = new PerfService{new MySQL{}, new NetworkMonitor{}};
     s->start();
     s->check();
     s->stop();
@@ -79,5 +104,6 @@ int main() {
     s->start();
     s->check();
     s->stop();
+    s->setMonitor(new MemoryMonitor{});
     return 0;
 }
